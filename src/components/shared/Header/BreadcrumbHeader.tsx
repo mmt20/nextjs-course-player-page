e68eb69b07845749da168f89913ad8f213b960ae
Mostafa Mohamed <mmt202002@gmail.com>
2025-10-15 05:02:37 +0300

    feat: implement BreadcrumbHeader component

---
+++ src/components/shared/Header/BreadcrumbHeader.tsx
@@ -0,0 +1,46 @@
+import React from "react";
+import Link from "next/link";
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+
+interface Breadcrumb {
+  label: string;
+  href?: string | null;
+}
+
+interface BreadcrumbHeaderProps {
+  breadcrumbs?: Breadcrumb[];
+  title: string;
+}
+
+const BreadcrumbHeader = ({ breadcrumbs, title }: BreadcrumbHeaderProps) => {
+  return (
+    <header className="px-4 py-6 border-b border-gray-200 bg-white">
+      <Breadcrumb>
+        <BreadcrumbList>
+          {breadcrumbs?.map((crumb, index) => (
+            <BreadcrumbItem>
+              <React.Fragment key={index}>
+                {crumb.href ? (
+                  <BreadcrumbLink href={crumb.href}>{crumb.label}</BreadcrumbLink>
+                ) : (
+                  <BreadcrumbPage>{crumb.label}</BreadcrumbPage>
+                )}
+                {index < breadcrumbs.length - 1 && <BreadcrumbSeparator />}
+              </React.Fragment>
+            </BreadcrumbItem>
+          ))}
+        </BreadcrumbList>
+      </Breadcrumb>
+      <h1 className="mt-4 text-3xl sm:text-4xl font-bold text-gray-900">{title}</h1>
+    </header>
+  );
+};
+
+export default BreadcrumbHeader;
